152240e5e83bb09a53cc5f3787fb469c1fd1d7ce
Simon Tucker <simon@brotherlogic@gmail>
2019-07-30 08:48:35 -0700

    Runs background task for transfers. This closes #10.

--- networkspeedapi.go
+++ networkspeedapi.go
@@ -6,11 +6,11 @@ import pb "github.com/brotherlogic/networkspeed/proto"
 
 //MakeTransfer responds to a given transfer request
 func (s *Server) MakeTransfer(ctx context.Context, req *pb.TransferRequest) (*pb.TransferResponse, error) {
-	return &pb.TransferResponse{}, nil
+	return &pb.TransferResponse{MessageSize: int64(len(req.Payload))}, nil
 }
 
 //RecordTransfer records a transfer
 func (s *Server) RecordTransfer(ctx context.Context, req *pb.RecordRequest) (*pb.RecordResponse, error) {
-	s.config.Transfers = append(s.config.Transfers, req.Transfer)
+	s.addTransfer(req.Transfer)
 	return &pb.RecordResponse{}, nil
 }
--- networkspeedutils.go
+++ networkspeedutils.go
@@ -1,7 +1,41 @@
 package main
 
-import pb "github.com/brotherlogic/networkspeed/proto"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"golang.org/x/net/context"
+
+	pb "github.com/brotherlogic/networkspeed/proto"
+)
 
 func (s *Server) addTransfer(t *pb.Transfer) {
 	s.config.Transfers = append(s.config.Transfers, t)
+}
+
+func (s *Server) runTransfer(ctx context.Context) error {
+	servers, err := s.bridge.getServers(ctx)
+	if err != nil {
+		return err
+	}
+
+	// Process the list randomly
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	for _, i := range r.Perm(len(servers)) {
+		t := time.Now()
+		resp, err := s.bridge.makeTransfer(ctx, servers[i])
+		if err == nil {
+			s.bridge.recordTransfer(ctx, &pb.Transfer{Destination: servers[i], Origin: s.Registry.Identifier, MessageSize: resp.MessageSize, TimeInMicroseconds: time.Now().Sub(t).Nanoseconds()})
+			return nil
+		}
+	}
+
+	return fmt.Errorf("Unable to find any suitable servers from list of %v", len(servers))
+}
+
+func buildPayload(sizeInBytes int) []byte {
+	resp := make([]byte, sizeInBytes)
+	rand.Read(resp)
+	return resp
 }
